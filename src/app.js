03bf182cd22d29b765546de82432512f42828fb5
kamogelo mogasoa <120066012+Kamogelo-7@users.noreply.github.com>
2025-02-09 05:16:41 +0200

    Added speech app.js

--- src/app.js
+++ src/app.js
@@ -23,6 +23,15 @@ async function fetchPokemonData() {
       const data = await res.json();
       const pokemonSprite = data.sprites.front_default;
 
+      const utterance = new SpeechSynthesisUtterance(`It's ${data.name}`);
+
+      speechSynthesis.cancel(); // Stops any ongoing speech before speaking new text
+
+      utterance.rate = 0.8;  // Normal speed
+      utterance.pitch = 0.7; // Lower pitch
+      utterance.volume = 1;  // Ensure full volume
+      speechSynthesis.speak(utterance);
+
       const imgElement = document.querySelector(".pokemonImg");
       imgElement.src = pokemonSprite;
       imgElement.style.display = "block";
